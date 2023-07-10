4f5dcae939f7ccfc2598b35a384c7782a8606670
Lisa Tsipris <dumisel@gmail.com>
2023-07-10 14:33:27 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  const corsOptions = {
-    origin: ['http://kpd.dumisel.nomoredomains.work', 'https://kpd.dumisel.nomoredomains.work'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['authorization', 'content-type'],
-  };
-  app.enableCors(corsOptions);
+
   await app.listen(4000);
 }
 bootstrap();
